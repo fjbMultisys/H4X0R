ec53d203c1544778c45429ab4d95b6d4f9ffba48
Jay Bergonia <jaybergonia@Jays-Mac-mini.local>
2022-01-10 10:42:05 +0800

    sample done

---
+++ H4X0R/Views/WebView.swift
@@ -0,0 +1,28 @@
+//
+//  WebView.swift
+//  H4X0R
+//
+//  Created by Jay Bergonia on 1/10/22.
+//
+
+import Foundation
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+}
